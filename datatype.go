2828e16650756175c766c1190b1ea2c986d44074
nateguy <66925023+herdodp@users.noreply.github.com>
2024-02-28 22:10:31 +0700

    data type

---
+++ datatype.go
@@ -0,0 +1,27 @@
+package main
+import "fmt"
+
+func main(){
+
+// there is 4 data type. string, int, float32, bool
+
+// string
+var name string = "herdo dimas pratirto"
+
+// int :
+// int, int8, int16, int32, int64
+var age int = 30
+
+// float32 :
+// float 32, float64
+const ipk float32 = 3.76
+
+// bool :
+// true and false
+var anakBungsu bool = true
+
+fmt.Print(name,"\n", age,"\n", ipk,"\n", anakBungsu)
+
+
+
+}
